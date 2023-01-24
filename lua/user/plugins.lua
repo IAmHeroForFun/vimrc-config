04ced9f60ef585e1221d28189e6e24c432151e17
IAmHeroForFun <everythinga1360@gmail.com>
2023-01-24 12:25:32 +0530

    updated toggle term & added animate plug

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,7 +96,8 @@ return packer.startup(function(use)
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" })
-
+	use({ "echasnovski/mini.nvim" })
+	use({ "echasnovski/mini.animate" })
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
